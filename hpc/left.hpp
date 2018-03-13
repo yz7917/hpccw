07e8c9e6c9d9f719987bf817f8281a6c35ccdbd8
joe <yz7917@ic.ac.uk>
2018-03-13 22:40:36 +0000

    git commit -m

---
+++ hpc/left.hpp
@@ -0,0 +1,14 @@
+//
+//  left.hpp
+//  hpc
+//
+//  Created by YIPENG ZHOU on 2018/3/13.
+//  Copyright © 2018年 joe. All rights reserved.
+//
+
+#ifndef left_hpp
+#define left_hpp
+
+#include <stdio.h>
+
+#endif /* left_hpp */
---
+++ hpc/right.hpp
@@ -0,0 +1,14 @@
+//
+//  right.hpp
+//  hpc
+//
+//  Created by YIPENG ZHOU on 2018/3/13.
+//  Copyright © 2018年 joe. All rights reserved.
+//
+
+#ifndef right_hpp
+#define right_hpp
+
+#include <stdio.h>
+
+#endif /* right_hpp */
